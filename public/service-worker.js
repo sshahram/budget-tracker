9fa4c1a2d5a6ae86fec8f66f6fce6783498a9e78
sshahram <shirin.shahram23@gmail.com>
2021-06-05 12:16:11 -0700

    intercept fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_PREFIX = 'FoodFest-';
+const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -6,15 +6,34 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./css/style.css",
     "./js/idb.js",
-    "./js/index.js",
+    "./js/index.js"
 ];
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if caches is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too
+            // return request || fetch(e.request)
+        })
+    )
+})
 
 // Cache resources
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache: ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+            return cache.addAll(FILES_TO_CACHE);
         })
     )
 })
